c0935401cdfe758a2332cf58bd517abc2d2a4bed
Artem Pavlenko <64309578+Artem817@users.noreply.github.com>
2024-06-04 20:24:03 +0300

    Update task1.cs

--- task1.cs
+++ task1.cs
@@ -1,89 +1,78 @@
 using System;
 
+public delegate void AttackEventHandler(Mage attacker, Mage defender, int damage);
+public delegate void DefenseEventHandler(Mage defender, int damage);
+
 public abstract class Mage
 {
-    public string Name { get; protected set; }
-    public int MagicLevel { get; protected set; }
-    public int Health { get; protected set; }
+    public string Name { get; set; }
+    public int MagicLevel { get; set; }
+    public int Health { get; set; }
 
-    protected Mage(string name, int magicLevel, int health)
+    public event AttackEventHandler Attacked;
+    public event DefenseEventHandler Defended;
+
+    public Mage(string name, int magicLevel)
     {
         Name = name;
         MagicLevel = magicLevel;
-        Health = health;
+        Health = 100;
     }
 
-    public abstract void Attack(Mage target);
+    public abstract void Attack(Mage other);
     public abstract void Defend(int damage);
+
+    public void OnAttacked(Mage defender, int damage)
+    {
+        Attacked?.Invoke(this, defender, damage);
+    }
+
+    public void OnDefended(int damage)
+    {
+        Defended?.Invoke(this, damage);
+    }
+
+    public bool IsAlive()
+    {
+        return Health > 0;
+    }
 }
 
 public interface ISpell
 {
-    void CastSpell(Mage caster, Mage target);
+    int Cast();
+}
+
+public class FireSpell : ISpell
+{
+    public int Cast()
+    {
+        return 20;
+    }
 }
 
 public class FireMage : Mage
 {
-    public FireMage(string name) : base(name, 10, 100) {}
+    private ISpell Spell;
 
-    public override void Attack(Mage target)
+    public FireMage(string name) : base(name, 5)
     {
-        Console.WriteLine($"{Name} casts a fireball at {target.Name}!");
-        target.Defend(20);
+        Spell = new FireSpell();
+    }
+
+    public override void Attack(Mage other)
+    {
+        int damage = Spell.Cast();
+        Console.WriteLine($"{Name} атакує вогнем на {damage} пошкодження!");
+        other.Defend(damage);
+        OnAttacked(other, damage);
     }
 
     public override void Defend(int damage)
     {
         Health -= damage;
-        Console.WriteLine($"{Name} takes {damage} damage and now has {Health} health left.");
-    }
-}
-
-// Клас Водяного мага
-public class WaterMage : Mage
-{
-    public WaterMage(string name) : base(name, 10, 100) {}
-
-    public override void Attack(Mage target)
-    {
-        Console.WriteLine($"{Name} casts a water blast at {target.Name}!");
-        target.Defend(15);
-    }
-
-    public override void Defend(int damage)
-    {
-        Health -= damage;
-        Console.WriteLine($"{Name} takes {damage} damage and now has {Health} health left.");
-    }
-}
-
-// Логіка гри
-public class Game
-{
-    private Mage player1;
-    private Mage player2;
-
-    public Game(Mage mage1, Mage mage2)
-    {
-        player1 = mage1;
-        player2 = mage2;
-    }
-
-    public void StartBattle()
-    {
-        while (player1.Health > 0 && player2.Health > 0)
-        {
-            player1.Attack(player2);
-            if (player2.Health > 0)
-            {
-                player2.Attack(player1);
-            }
-        }
-
-        if (player1.Health > 0)
-            Console.WriteLine($"{player1.Name} wins!");
-        else
-            Console.WriteLine($"{player2.Name} wins!");
+        Console.WriteLine($"{Name} отримав {damage} пошкоджень i має {Health} здоровя.");
+        OnDefended(damage);
     }
 }
 
@@ -91,10 +80,42 @@ class Program
 {
     static void Main()
     {
-        Mage fireMage = new FireMage("Merlin");
-        Mage waterMage = new WaterMage("Morgana");
+        Mage player = new FireMage("Маг-гравець");
+        Mage computer = new FireMage("Комп'ютерний маг");
 
-        Game game = new Game(fireMage, waterMage);
-        game.StartBattle();
+        player.Attacked += (attacker, defender, damage) => Console.WriteLine($"Event: {attacker.Name} атакував {defender.Name} на {damage} пошкодження!");
+        computer.Defended += (defender, damage) => Console.WriteLine($"Event: {defender.Name} захистився від {damage} пошкоджень!");
+
+        Console.WriteLine("Ласкаво просимо на Битву магів!!");
+        Console.WriteLine("Ви контролюєте мага-гравця. Твiй суперник - комп'ютерний маг.");
+
+        while (player.IsAlive() && computer.IsAlive())
+        {
+            Console.WriteLine("Виберіть свою дію:\n1. Атакувати\n2. Пропустити хід");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                player.Attack(computer);
+            }
+            else
+            {
+                Console.WriteLine("Ви вирішили пропустити свій хiд.");
+            }
+
+            if (computer.IsAlive())
+            {
+                computer.Attack(player);
+            }
+        }
+
+        if (player.IsAlive())
+        {
+            Console.WriteLine("Вітаю! Ти виграв!");
+        }
+        else
+        {
+            Console.WriteLine("Game Over! Комп'ютер переміг.");
+        }
     }
 }
